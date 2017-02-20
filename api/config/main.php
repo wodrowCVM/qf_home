4e2598226848dc456d42662fb3026ba05e16a572
wodrow-home <1173957281@qq.com>
2017-02-20 14:55:03 +0800

    init

--- api/config/main.php
+++ api/config/main.php
@@ -7,23 +7,23 @@ $params = array_merge(
 );
 
 $config = [
-    'id' => 'frontend',
+    'id' => 'api',
     'basePath' => dirname(__DIR__),
     'name' => "轻纺家园",
     'bootstrap' => ['log'],
-    'controllerNamespace' => 'frontend\controllers',
+    'controllerNamespace' => 'api\controllers',
     'components' => [
         'request' => [
-            'csrfParam' => '_csrf-frontend',
+            'csrfParam' => '_csrf-api',
         ],
         'user' => [
             'identityClass' => 'common\models\User',
             'enableAutoLogin' => true,
-            'identityCookie' => ['name' => '_identity-frontend', 'httpOnly' => true],
+            'identityCookie' => ['name' => '_identity-api', 'httpOnly' => true],
         ],
         'session' => [
-            // this is the name of the session cookie used for login on the frontend
-            'name' => 'advanced-frontend',
+            // this is the name of the session cookie used for login on the api
+            'name' => 'advanced-api',
         ],
         'log' => [
             'traceLevel' => YII_DEBUG ? 3 : 0,
