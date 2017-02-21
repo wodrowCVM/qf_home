97ccf59d2df00dff6400505f42adacc229630385
wodrow-home <1173957281@qq.com>
2017-02-21 17:21:44 +0800

    init

--- test/controllers/SiteController.php
+++ test/controllers/SiteController.php
@@ -75,6 +75,7 @@ class SiteController extends Controller
     {
         Tools::_vp(Yii::$app->request->get(),0,2);
         Tools::_vp(Yii::$app->request->post(),0,3);
+        Tools::_vp(Yii::$app->wechat,0,3);
         if(\Yii::$app->wechat->isWechat && !\Yii::$app->wechat->isAuthorized()) {
             return \Yii::$app->wechat->authorizeRequired()->send();
         }
