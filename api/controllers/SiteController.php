710e34c1a9976c10f58ec196ec8716ab34714b07
wodrow-home <1173957281@qq.com>
2017-02-21 09:31:33 +0800

    init

--- api/controllers/SiteController.php
+++ api/controllers/SiteController.php
@@ -1,67 +1,23 @@
 <?php
 namespace api\controllers;
 
-use Yii;
-use yii\base\InvalidParamException;
-use yii\web\BadRequestHttpException;
-use yii\web\Controller;
-use yii\filters\VerbFilter;
-use yii\filters\AccessControl;
-use common\models\LoginForm;
-use api\models\PasswordResetRequestForm;
-use api\models\ResetPasswordForm;
-use api\models\SignupForm;
-use api\models\ContactForm;
-
 /**
  * Site controller
  */
 class SiteController extends \deepziyu\yii\rest\Controller
 {
     /**
-     * @inheritdoc
+     * 1 Displays test1
+     * @desc this is a desc
+     * @param int $id 请求参数
+     * @return int id id
+     * @return string x x-id
      */
-    public function behaviors()
+    public function actionTest1($id=1)
     {
         return [
-            'access' => [
-                'class' => AccessControl::className(),
-                'only' => ['logout', 'signup'],
-                'rules' => [
-                    [
-                        'actions' => ['signup'],
-                        'allow' => true,
-                        'roles' => ['?'],
-                    ],
-                    [
-                        'actions' => ['logout'],
-                        'allow' => true,
-                        'roles' => ['@'],
-                    ],
-                ],
-            ],
-            'verbs' => [
-                'class' => VerbFilter::className(),
-                'actions' => [
-                    'logout' => ['post'],
-                ],
-            ],
-        ];
-    }
-
-    /**
-     * @inheritdoc
-     */
-    public function actions()
-    {
-        return [
-            'error' => [
-                'class' => 'yii\web\ErrorAction',
-            ],
-            'captcha' => [
-                'class' => 'yii\captcha\CaptchaAction',
-                'fixedVerifyCode' => YII_ENV_TEST ? 'testme' : null,
-            ],
+            'id'=>$id,
+            'x'=>"x-".$id,
         ];
     }
 
@@ -74,21 +30,6 @@ class SiteController extends \deepziyu\yii\rest\Controller
     {
         return [
             'key' => 'value',
-        ];
-    }
-
-    /**
-     * 1 Displays test1
-     * @desc this is a desc
-     * @param int $id 请求参数 others
-     * @return int id id
-     * @return string x x-id
-     */
-    public function actionTest1($id=1)
-    {
-        return [
-            'id'=>$id,
-            'x'=>"x-".$id,
         ];
     }
 }
