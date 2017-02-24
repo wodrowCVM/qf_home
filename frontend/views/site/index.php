fb89a051cb63b9f7516f4f504236eb7fadf4585f
wodrow-home <1173957281@qq.com>
2017-02-24 16:04:51 +0800

    init

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -9,7 +9,7 @@ $this->title = Yii::$app->name;
 
     <div id="categorys_and_ads">
         <div class="row">
-            <div class="col-sm-3 hidden-xs">
+            <div class="col-lg-3 hidden-md hidden-sm hidden-xs">
                 <div class="category_1">
                     <ol class="nav">
                         <li>
@@ -21,7 +21,7 @@ $this->title = Yii::$app->name;
                             </div>
                         </li>
                         <li>
-                            <a href="#">墙纸<span class="pull-right"><?=\kartik\icons\Icon::show('chevron-right') ?></i></span></a>
+                            <a href="#">墙布<span class="pull-right"><?=\kartik\icons\Icon::show('chevron-right') ?></i></span></a>
                             <div class="category_2">
                                 <div class="category_3">
                                     墙纸
@@ -36,21 +36,45 @@ $this->title = Yii::$app->name;
                                 </div>
                             </div>
                         </li>
-                    </ol>
+                        <li>
+                            <a href="#">原料<span class="pull-right"><?=\kartik\icons\Icon::show('chevron-right') ?></i></span></a>
+                            <div class="category_2">
+                                <div class="category_3">
+                                    桌布
+                                </div>
+                            </div>
+                        </li>
+                        <li>
+                            <a href="#">窗帘<span class="pull-right"><?=\kartik\icons\Icon::show('chevron-right') ?></i></span></a>
+                            <div class="category_2">
+                                <div class="category_3">
+                                    桌布
+                                </div>
+                            </div>
+                        </li>
+                        <li>
+                            <a href="#">浴巾<span class="pull-right"><?=\kartik\icons\Icon::show('chevron-right') ?></i></span></a>
+                            <div class="category_2">
+                                <div class="category_3">
+                                    桌布
+                                </div>
+                            </div>
+                        </li>
                 </div>
             </div>
-            <div class="col-sm-9">
+            <div class="col-lg-9">
                 <div class="advertisement">
                     <?php
                     echo \yii\bootstrap\Carousel::widget([
                         'controls' => [
-                            '<sp
-an class="glyphicon glyphicon-chevron-left"></span>',
+                            '<span class="glyphicon glyphicon-chevron-left"></span>',
                             '<span class="glyphicon glyphicon-chevron-right"></span>',
                         ],
                         'items'=>[
-                            ['content' => \yii\helpers\Html::img('http://img.mp.itc.cn/upload/20160630/6398240c412a453a8a3a5d2e2e51301a_th.jpg')],
-                            ['content' => \yii\helpers\Html::img('http://img.mp.itc.cn/upload/20160630/c7e87816797f4a7ab9751e5b0c9ae438_th.jpg')],
+                            ['content' => \yii\helpers\Html::img('http://7xsbq6.com1.z0.glb.clouddn.com/1000480.png')],
+                            ['content' => \yii\helpers\Html::img('http://7xsbq6.com1.z0.glb.clouddn.com/1000480.png')],
+                            ['content' => \yii\helpers\Html::img('http://7xsbq6.com1.z0.glb.clouddn.com/1000480.png')],
+                            ['content' => \yii\helpers\Html::img('http://7xsbq6.com1.z0.glb.clouddn.com/1000480.png')],
                         ],
                     ]);
                     ?>
