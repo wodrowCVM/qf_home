35147ffe0bf6b3eb1e261aa351704657b25551e5
wodrow-home <1173957281@qq.com>
2017-03-20 13:26:01 +0800

    init

--- test/controllers/SiteController.php
+++ test/controllers/SiteController.php
@@ -74,24 +74,73 @@ class SiteController extends Controller
      */
     public function actionIndex()
     {
-        /*Tools::_vp(Yii::$app->request->get(),0,2);
-        Tools::_vp(Yii::$app->request->post(),0,3);
-        Tools::_vp(Yii::$app->wechat,0,3);
-        Tools::_vp(\Yii::$app->wechat->isWechat,0,3);
-        Tools::_vp(!\Yii::$app->wechat->isAuthorized(),0,3);
-        Tools::_vp(\Yii::$app->wechat->authorizeRequired()->send(),0,3);
-        if(\Yii::$app->wechat->isWechat && !\Yii::$app->wechat->isAuthorized()) {
-            return \Yii::$app->wechat->authorizeRequired()->send();
-        }*/
-        $options = Yii::$app->params['WECHAT'];
-        $app = new \EasyWeChat\Foundation\Application($options);
-        $response = $app->server->serve();
-// 将响应输出
-//        $response->send();
-//        Tools::_vp(Yii::$app->request->get(),0,2);
-//        Tools::_vp(Yii::$app->request->post(),0,3);
-//        Tools::_vp($response,0,3);
-        return $response;
+        ///获得参数 signature nonce token timestamp echostr
+        $nonce     = $_GET['nonce'];
+        $token     = 'wodrow';
+        $timestamp = $_GET['timestamp'];
+        $echostr   = $_GET['echostr'];
+        $signature = $_GET['signature'];
+        //形成数组，然后按字典序排序
+        $array = array();
+        $array = array($nonce, $timestamp, $token);
+        sort($array);
+        //拼接成字符串,sha1加密 ，然后与signature进行校验
+        $str = sha1( implode( $array ) );
+        if( $str == $signature && $echostr ){
+            //第一次接入weixin api接口的时候
+            echo  $echostr;
+            exit;
+        }else{
+
+            //1.获取到微信推送过来post数据（xml格式）
+            $postArr = $GLOBALS['HTTP_RAW_POST_DATA'];
+            //2.处理消息类型，并设置回复类型和内容
+            /*<xml>
+    <ToUserName><![CDATA[toUser]]></ToUserName>
+    <FromUserName><![CDATA[FromUser]]></FromUserName>
+    <CreateTime>123456789</CreateTime>
+    <MsgType><![CDATA[event]]></MsgType>
+    <Event><![CDATA[subscribe]]></Event>
+    </xml>*/
+            $postObj = simplexml_load_string( $postArr );
+            //$postObj->ToUserName = '';
+            //$postObj->FromUserName = '';
+            //$postObj->CreateTime = '';
+            //$postObj->MsgType = '';
+            //$postObj->Event = '';
+            // gh_e79a177814ed
+            //判断该数据包是否是订阅的事件推送
+            if( strtolower( $postObj->MsgType) == 'event'){
+                //如果是关注 subscribe 事件
+                if( strtolower($postObj->Event == 'subscribe') ){
+                    //回复用户消息(纯文本格式)
+                    $toUser   = $postObj->FromUserName;
+                    $fromUser = $postObj->ToUserName;
+                    $time     = time();
+                    $msgType  =  'text';
+                    $content  = '欢迎关注我们的微信公众账号'.$postObj->FromUserName.'-'.$postObj->ToUserName;
+                    $template = "<xml>
+                                <ToUserName><![CDATA[%s]]></ToUserName>
+                                <FromUserName><![CDATA[%s]]></FromUserName>
+                                <CreateTime>%s</CreateTime>
+                                <MsgType><![CDATA[%s]]></MsgType>
+                                <Content><![CDATA[%s]]></Content>
+                                </xml>";
+                    $info     = sprintf($template, $toUser, $fromUser, $time, $msgType, $content);
+                    echo $info;
+                    /*<xml>
+                    <ToUserName><![CDATA[toUser]]></ToUserName>
+                    <FromUserName><![CDATA[fromUser]]></FromUserName>
+                    <CreateTime>12345678</CreateTime>
+                    <MsgType><![CDATA[text]]></MsgType>
+                    <Content><![CDATA[你好]]></Content>
+                    </xml>*/
+
+
+                }
+            }
+
+        }
     }
 
     /**
